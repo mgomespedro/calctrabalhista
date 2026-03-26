5e906c00c87c7fbec3101044aa38ca865af3b947
mgomespedro <mgomespedro@live.com>
2026-03-26 19:15:30 +0000

    fix 3: botão 'já sou premium'

--- src/components/ModalLoginPremium.jsx
+++ src/components/ModalLoginPremium.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { usePremium } from '../context/PremiumContext'
 
 export default function ModalLoginPremium({ onFechar }) {
